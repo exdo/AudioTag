7ca7e61e1a9d00693b83e3ed7e87d8e6fe02c05b
oywf <oywf802@126.com>
2024-07-14 21:47:12 +0800

    初始化

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module xyz.idaoteng.lesson1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens xyz.idaoteng.lesson1 to javafx.fxml;
+    exports xyz.idaoteng.lesson1;
+}
